840876f9e231e337316174fcce8aae65062df4bb
Rishank99 <111288921+Rishank99@users.noreply.github.com>
2024-02-17 00:00:39 +0530

    first commit

---
+++ 02-backend/spring-library/src/main/java/com/library/config/MyDataRestConfig.java
@@ -0,0 +1,46 @@
+package com.library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.library.entities.Book;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer{
+
+//	To make requests to our frontend
+	private String theAllowedOrigins="http://localhost:3000";
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+			                                         CorsRegistry cors) {
+		HttpMethod[] theUnsupportedActions = {
+				HttpMethod.POST,
+				HttpMethod.DELETE,
+				HttpMethod.PATCH,
+				HttpMethod.PUT};
+		config.exposeIdsFor(Book.class);
+		
+		disableHttpMethods(Book.class,config,theUnsupportedActions);
+		
+		/* Configure CORS Mapping */
+		cors.addMapping(config.getBasePath() + "/**")
+		      .allowedOrigins(theAllowedOrigins);
+	}
+	
+	
+	
+	private void disableHttpMethods(Class theClass, 
+			                        RepositoryRestConfiguration config,
+			                        HttpMethod[] theUnsupportedActions) {
+		config.getExposureConfiguration()
+		.forDomainType(theClass)
+		.withItemExposure((metdata, httpMethods) ->
+		        httpMethods.disable(theUnsupportedActions))
+		.withCollectionExposure((metdata, httpMethods) ->
+		        httpMethods.disable(theUnsupportedActions));
+	}
+}
